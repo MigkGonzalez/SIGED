f94d2b344eb46b51d8158cbfb9e0c9027c860a49
MigkGonzalez <mfgonzaleza@unal.edu.co>
2021-10-11 22:14:10 -0500

    Tercer commit, organizacion de componentes y vistas, navegacion con rutas

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
+      <Header></Header> 
+      
+      
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/Actualizar">Actualizar</router-link> |
+      <router-link to="/Crud">CrudE</router-link> 
+    </div> -->
     <router-view/>
+    <Inscripcion></Inscripcion>
+    <FooterComp></FooterComp>
   </div>
 </template>
 
+<script>
+import Header from './components/Header.vue'
+import Inscripcion from './components/Inscripcion.vue'
+import FooterComp from './components/FooterComp.vue'
+export default {
+  name: 'App',
+  
+  data: () => ({
+
+  }),
+  components:{
+    Header,
+    Inscripcion,
+    FooterComp,
+    }
+}
+</script>
+
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+} */
 </style>
---
+++ src/views/CrudEntrenador.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class:crudEntrenador>
+        <span></span>
+    </div>
+</template>
+<script>
+import Crud from './components/Crud.vue' 
+export default {
+    name: 'CRUD',
+  components: {
+      Crud
+    
+  }
+}
+</script>
+<style>
+
+</style>
